c4e7747707749119a2630f29b6ba22217cb97be4
Chris Michael <chris.perez@upr.edu>
2019-12-23 15:16:49 -0400

    composition function -> [useLogin] in use.

--- src/client/src/views/Login.vue
+++ src/client/src/views/Login.vue
@@ -59,21 +59,12 @@
 <script>
   import {ref} from '@vue/composition-api';
   import {useStore} from '@u3u/vue-hooks';
+  import {useLogin} from '../composition-functions/login';
 
   export default {
     name: 'Login',
     setup(){
-      const store = useStore();
-
-      const loginEmail = ref('');
-      const loginPass = ref('');
-
-      const login = () =>{
-        const user = new URLSearchParams();
-        user.append('email' , loginEmail.value);
-        user.append('password' , loginPass.value); 
-        store.value.dispatch('LOGIN_USER' , user);
-      };
+      const {loginEmail , loginPass , login} = useLogin();
 
       return{
         loginEmail,
