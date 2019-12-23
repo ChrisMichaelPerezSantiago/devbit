dbc2f1f9233c7f41437f8c97a8071fc89f48f51e
Chris Michael <chris.perez@upr.edu>
2019-12-23 13:16:28 -0400

    vue.config.js -> electronBuilder configuration

--- src/client/vue.config.js
+++ src/client/vue.config.js
@@ -1,3 +1,35 @@
+
 module.exports = {
-  lintOnSave: false
+  productionSourceMap: false,
+  lintOnSave: false,
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        appId: "com.devbit.app",
+        productName: "DevBit",
+        win: {
+          icon: "./public/logo.png",
+          target: [
+            {
+              target: "nsis",
+              arch: ["x64", "ia32"]
+            }
+          ]
+        },
+        nsis:{
+          oneClick: false,
+          perMachine: true,
+          allowToChangeInstallationDirectory: true
+        },
+        publish: [
+          {
+            provider: "github",
+            owner: "ChrisMichaelPerezSantiago",
+            repo: "devbit",
+            releaseType: "draft",
+          }
+        ]
+      }
+    }
+  }
 }
