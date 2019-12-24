822bfbb05c37ae9236b31eb9be76eabf19e0cf12
Chris Michael <chris.perez@upr.edu>
2019-12-23 20:50:18 -0400

    views/Home.vue -> basic implementation of the vertical navigation.

--- src/client/src/views/Home.vue
+++ src/client/src/views/Home.vue
@@ -1,11 +1,37 @@
 <template>
-  <div class="home">
-    <h1>Home</h1>
-  </div>
+  <main class="container">
+	  <aside class="servers">
+	  	<div class="servers-collection">
+	  		<div class="server focusable server-friends unread" role="button" aria-label="Friends unread">
+	  			<div class="server-icon"><i class="fas fa-user"></i></div>
+	  		</div>
+	  	</div>
+  
+	  	<div class="servers-collection">
+	  		<div class="server focusable unread" role="button" aria-label="user">
+	  			<div class="server-icon"><img src="#"/></div>
+	  		</div>
+	  	</div>
+  
+	  	<div class="servers-collection">
+	  		<div class="server focusable active" role="button" aria-label="My Server" aria-selected="true">
+	  			<div class="server-icon"><img src="#"/></div>
+	  		</div>
+	  	</div>
+	  </aside>
+  </main>
 </template>
 
 <script>
+  import {onMounted} from '@vue/composition-api';
+  import {focusableChannelsIcons} from '../utils/index';
+
   export default {  
     name: 'home', 
+    setup(){
+      onMounted(() =>{
+        focusableChannelsIcons();
+      });
+    }
   };
 </script>
