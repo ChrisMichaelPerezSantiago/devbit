b8f04c3dba9b8f838857e254006bb7e918376f88
Chris Michael <chris.perez@upr.edu>
2019-12-27 14:11:17 -0400

    views/Home.vue -> Indentation of 2 space

--- src/client/src/views/Home.vue
+++ src/client/src/views/Home.vue
@@ -1,24 +1,22 @@
 <template>
   <main class="container">
-	  <aside class="servers">
-	  	<div class="servers-collection">
-	  		<div class="server focusable server-friends unread" role="button" aria-label="Friends unread">
-	  			<div class="server-icon"><i class="fas fa-user"></i></div>
-	  		</div>
-	  	</div>
-  
-	  	<div class="servers-collection">
-	  		<div class="server focusable unread" role="button" aria-label="user">
-	  			<div class="server-icon"><img src="#"/></div>
-	  		</div>
-	  	</div>
-  
-	  	<div class="servers-collection">
-	  		<div class="server focusable active" role="button" aria-label="My Server" aria-selected="true">
-	  			<div class="server-icon"><img src="#"/></div>
-	  		</div>
-	  	</div>
-	  </aside>
+    <aside class="servers">
+      <div class="servers-collection">
+        <div class="server focusable server-friends unread" role="button" aria-label="Friends unread">
+          <div class="server-icon"><i class="fas fa-user"></i></div>
+        </div>
+      </div>
+      <div class="servers-collection">
+        <div class="server focusable unread" role="button" aria-label="user">
+          <div class="server-icon"><img src="#"/></div>
+        </div>
+      </div>
+      <div class="servers-collection">
+        <div class="server focusable active" role="button" aria-label="My Server" aria-selected="true">
+          <div class="server-icon"><img src="#"/></div>
+        </div>
+      </div>
+    </aside>
   </main>
 </template>
 
